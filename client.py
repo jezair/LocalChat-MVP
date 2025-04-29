4d4adee48a69340ec765e120a5aa54c0c171c035
jezair <150662647+jezair@users.noreply.github.com>
2025-04-29 17:18:36 +0300

    v1.0

---
+++ client.py
@@ -0,0 +1,26 @@
+from socket import *
+import threading
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+name = input("Введіть ім'я: ")
+client_socket.connect(('4.tcp.eu.ngrok.io', 19734))
+client_socket.send(name.encode())
+
+
+def send_message():
+   while True:
+       client_message = input()
+       if client_message.lower() == 'exit':
+           client_socket.close()
+           break
+       client_socket.send(client_message.encode())
+
+
+threading.Thread(target=send_message).start() # запуск потока відправки повідомлень
+while True:
+   try:
+       message = client_socket.recv(1024).decode().strip()
+       if message:
+           print(message)
+   except:
+       break
